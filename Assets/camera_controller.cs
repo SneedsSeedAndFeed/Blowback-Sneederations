99347fa695787a7e68c2a942ed99e100fa35fdc8
Jack <0319024@schoolsnet.act.edu.au>
2021-06-18 22:03:45 +1000

    player and selecting tiles
    the player does nothing and neither does selecting a tiles, but it gives the illusion that something is in fact happening

--- Assets/camera_controller.cs
+++ Assets/camera_controller.cs
@@ -14,17 +14,37 @@ public class camera_controller : MonoBehaviour
         myCamera = GetComponent<Camera>();
     }
 
+    
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1) || Input.GetMouseButton(2))
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(2)) // if rmb or mmb are pressed you can move the camera
         {
             transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
                                        Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0.0f);
         }
 
-        myCamera.fieldOfView += zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+        myCamera.fieldOfView += zoomSpeed * Input.GetAxis("Mouse ScrollWheel");//the fov and orthographicsize(basically the zoom of the camera) are changed with scroll wheel
         myCamera.orthographicSize += zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+
+        myCamera.orthographicSize = Mathf.Clamp(myCamera.orthographicSize, 4, 12);
+        //maximum and minimum zoom
+
+        float xpos = transform.position.x;
+        float ypos = transform.position.y;
+
+        xpos = Mathf.Clamp(xpos, -25 + myCamera.orthographicSize + 1.1f * (myCamera.orthographicSize - 5),
+            29 - myCamera.orthographicSize - 1.1f * (myCamera.orthographicSize - 5));
+
+       
+        ypos = Mathf.Clamp(ypos, -28 + myCamera.orthographicSize,
+            20 - myCamera.orthographicSize);
+
+        //these two are to stop the camera from moving outside the area of the map, not made for random maps yet,
+        //however it looks complicated, this is because it is made such that zooming wont change the amount you can see outside the map
+
+        transform.position = new Vector3(xpos, ypos, transform.position.z);
 
     }
 }
--- Assets/tile_manager.cs
+++ Assets/tile_manager.cs
@@ -8,7 +8,11 @@ public class tile_manager : MonoBehaviour
 {
     private Grid grid;
     [SerializeField] private Tilemap interactiveMap = null;
+    [SerializeField] private Tilemap groundMap = null;
+    [SerializeField] private Tilemap playerMap = null;
     [SerializeField] private Tile hoverTile = null;
+    [SerializeField] private Tile selectedTile = null;
+    [SerializeField] private Tile playerTile = null;
 
 
     private Vector3Int previousMousePos = new Vector3Int();
@@ -16,6 +20,9 @@ public class tile_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector3Int randpos = new Vector3Int(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+        playerMap.SetTile(randpos, playerTile);
+        //drops player in some random spot
         grid = gameObject.GetComponent<Grid>();
     }
 
@@ -24,11 +31,21 @@ public class tile_manager : MonoBehaviour
     {
         // Mouse over -> highlight tile
         Vector3Int mousePos = GetMousePosition();
-        if (!mousePos.Equals(previousMousePos))
-        {
-            interactiveMap.SetTile(previousMousePos, null); // Remove old hoverTile
-            interactiveMap.SetTile(mousePos, hoverTile);
+        interactiveMap.SetTile(previousMousePos, null);
+        if (groundMap.HasTile(mousePos))
+        { // Remove old hoverTile
+            if (Input.GetMouseButton(0))
+            {
+                interactiveMap.SetTile(mousePos, selectedTile);
+                //if you press lmb, the tile where your mouse is becomes "selected" whatever the fuck that means
+            }
+            else
+            {
+                interactiveMap.SetTile(mousePos, hoverTile);
+                //simply hovering your mouse over a tile will make it piss yellow
+            }
             previousMousePos = mousePos;
+
         }
         
     }
